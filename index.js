f5cbb197f1d28f6ffedd64a03aee5aca4182b928
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2023-12-21 23:54:15 +0900

    number.string

--- index.js
+++ index.js
@@ -2,7 +2,91 @@
 // juft va toq son
 
 function evenOrOdd(number) {
-    return number % 2 === 0 ? "Even" : "Odd";
-  }
-  console.log(evenOrOdd);
-  
+  return number % 2 === 0 ? "Even" : "Odd";
+}
+console.log(evenOrOdd);
+
+// 2-masala
+
+multiply = function (a, b) {
+  return a * b;
+};
+
+// 3-masala
+
+function positiveSum(arr) {
+  // return arr.reduce((a, b) => a + (b > 0 ? b : 0));
+}
+
+console.log(positiveSum());
+// 4-masala
+
+function makeNegative(number) {
+  // Agar raqam salbiy bo'lmasa, salbiy qilinganini qaytaradi
+  // return number < 0 ? number : -number;
+  return -Math.abs(number);
+}
+
+// Test qilish
+console.log(makeNegative(1)); // -1
+console.log(makeNegative(-5)); // -5
+console.log(makeNegative(0)); // 0
+console.log(makeNegative(0.12)); // -0.12
+
+// 5-masala
+
+let number = 12;
+function opposite(number) {
+  return number * -1;
+}
+
+console.log(opposite(number));
+
+// 6-masala
+let num = 123
+function numb(num) {
+  return num.toString();
+}
+console.log(numb(typeof 'num'));
+
+// 7-masala
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   -Masala
+
+// const points=games=>games.reduce((output,current)=>{
+//     return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+//   },0)
+
+// // function points(games) {
+// //   return games.reduce((output,current)=>{
+// //     let x = parseInt(current[0]);
+// //     let y = parseInt(current[2]);
+// //     let value= x>y ? 3 : x===y ? 1 : 0;
+// //     return output+value;
+// //   },0)
+// // }
+
+// function points(games) {
+//     let total = 0;
+//     games.map(game => {
+//       if (game[0] > game[2]) {
+//         total += 1;
+//       } else if (game[0] > game[2]) {
+//         total += 3;
+//       }
+//     });
+//     return total;
+//   }
