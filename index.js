f505ecf17b90d69c6436ea9e5e9cd27a8eaa6be5
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-01-04 13:20:40 +0900

    Count of positives / sum of negatives

--- index.js
+++ index.js
@@ -454,6 +454,21 @@ function simpleMultiplication(number) {
 }
  console.log(simpleMultiplication(2));
 
+// 33-masala //Count of positives / sum of negatives
+let input =  [1, 2, 3, 4, 5, 6,20, 7, 8, 9, 10, -11, -12, -13, -14, -25]
+function countPositivesSumNegatives(input) {
+  // your code here
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
+console.log(countPositivesSumNegatives(input));
+
 
 
 
