e9310c5910efaa0f3350d8e116876b1651b01262
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-02-06 23:52:00 +0900

    Set Alarm

--- index.js
+++ index.js
@@ -776,6 +776,23 @@ function updateLight(current) {
   }
 
 }
+
+// 51-masala
+
+// 1-usul
+
+function setAlarm(employed, vacation){
+  return (employed && !vacation) ? true : false;
+
+}
+
+// 2-usul
+
+function setAlarm(employed, vacation){
+  return employed > vacation
+}
+
+console.log(setAlarm());
 // const points=games=>games.reduce((output,current)=>{
 //     return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
 //   },0)
