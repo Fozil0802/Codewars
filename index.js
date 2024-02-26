0c9281f3c0d09b70346b698766fc45b68a3cd466
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-02-26 23:29:46 +0900

    Switch it Up

--- index.js
+++ index.js
@@ -1061,6 +1061,30 @@ function correct(s){
   return s.join('');
 }
 console.log(correct());
+
+// 66-masala // Switch it Up!
+
+// 1-usul
+function switchItUp(number){
+//Write your own Code!
+   return ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'][number];
+}
+// 2-usul
+function switchItUp(number) {
+  switch (number) {
+    case 0: return 'Zero';
+    case 1: return 'One';
+    case 2: return 'Two';
+    case 3: return 'Three';
+    case 4: return 'Four';
+    case 5: return 'Five';
+    case 6: return 'Six';
+    case 7: return 'Seven';
+    case 8: return 'Eight';
+    case 9: return 'Nine';
+  }
+}
+console.log(switchItUp());
 // const points=games=>games.reduce((output,current)=>{
 //  return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
 //   },0)
