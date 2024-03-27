d2f9ead00076d5ff0a1eeef28542930199bec569
Fozil0208 <xudoyberdiyev1995fozil@gmail.com>
2024-03-27 18:49:31 +0900

    Unfinished Loop

--- index.js
+++ index.js
@@ -1438,6 +1438,28 @@ function move (position, roll) {
 
 move();
 
+// 82-masala //Unfinished Loop - Bug Fixing #1
+
+// 1-usul
+
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
+
+// 2-usul
+
+function createArray(number){
+  return Array.from({length:number},(v,i)=>i+1)
+}
+
+createArray();
+
 // const points=games=>games.reduce((output,current)=>{
 //  return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
 //   },0)
